20faf1f30603427d2842d3a047e8b3c8797ecb63
stefano <sirfoga@protonmail.com>
2016-11-10 14:25:10 +0100

    removed files.manager => updated dir and file classes

--- hal/files/models.py
+++ hal/files/models.py
@@ -20,10 +20,227 @@
 
 
 import os
-from hal.files.manager import is_hidden
+import re
+from send2trash import send2trash
 
 
-class Document:
+BAD_CHARS = [
+    ".", ":", "\"", "’", "&", "720p", "1080p", "yify", ",", "\"S", "brrip", "bluray", "Bokutox", "x264", "[", "]", "sparks", "h264",
+    "aac", "ozlem", "ac3", "ozlem", "etrg", "dvdrip", "xvid", "nydic", "sujaidr", "x265", "hevc", "(pimprg)", "aac",
+    "ozlem", "remastered", "anoxmous", "yts"]
+RUSSIAN_CHARS = ["ш", "а", "б", "л", "о", "н", "ы", "р", "е", "а", "л", "и", "з", "а", "ц", "и", "и", "к",
+    "о", "р", "п", "о", "р", "а", "т", "и", "в", "н", "ы", "х", "п", "р", "и", "л", "о", "ж", "е", "н", "и", "й",
+    "в", "о", "п", "р", "о", "с", "ы", "и", "о", "т", "в", "е", "т", "ы", "п", "о", "б", "е", "з", "о", "п",
+    "а", "с", "н", "о", "с", "т", "и", "д", "а", "н", "н", "ы", "х"]
+VIDEO_FORMAT = [".", ".3g2", ".3gp", ".amv", ".asf", ".avi", ".drc", ".f4a", ".f4b", ".f4p", ".f4v", ".flv",
+    ".gifv", ".m2v", ".m4p", ".m4v", ".mkv", ".mng", ".mov", ".mp2", ".mp4", ".mpe", ".mpeg", ".mpg", ".mpv", ".mxf",
+    ".nsv", ".ogg", ".ogv", ".qt", ".rm", ".rmvb", ".roq", ".svi", ".vob", ".webm", ".wmv", ".yuv"]
+ARCHIVE_FORMAT = [".7z", ".??_", ".?Q?", ".?Z?", ".a", ".ace", ".afa", ".alz", ".apk", ".ar", ".arc", ".arj",
+                      ".b1", ".ba", ".bh", ".bz2", ".cab", ".car", ".cfs", ".cpio", ".cpt", ".dar", ".dd", ".dgc",
+                      ".dmg", ".ear", ".ecc", ".F", ".gca", ".gz", ".ha", ".hki", ".ice", ".infl", ".iso", ".jar",
+                      ".kgb", ".LBR", ".lbr", ".lha", ".lz", ".lzh", ".lzma", ".lzo", ".lzx", ".mar", ".pak", ".paq6",
+                      ".paq7", ".paq8", ".par", ".par2", ".partimg", ".pea", ".pim", ".pit", ".qda", ".rar", ".rk",
+                      ".rz", ".s7z", ".sda", ".sea", ".sen", ".sfark", ".sfx", ".shar", ".shk", ".sit", ".sitx",
+                      ".sqx", ".sz", ".tar", ".tar.bz2", ".tar.gz", ".tar.lzma", ".tar.Z", ".tbz2", ".tgz", ".tlz",
+                      ".uc", ".uc0", ".uc2", ".uca", ".ucn", ".ue2", ".uha", ".ur2", ".war", ".wim", ".xar", ".xp3",
+                      ".xz", ".yz1", ".z", ".Z", ".zip", ".zipx", ".zoo", ".zpaq", ".zz"]
+SUBTITLE_FORMAT = [".srt", ".sub", ".sbv"]
+TEXT_FORMAT = [".cnf", ".conf", ".cfg", ".chm", ".epub", ".log", ".asc", ".txt", ".url"]
+IMAGE_FORMAT = [".ani", ".bmp", ".cal", ".fax", ".gif", ".img", ".jbg", ".jpe", ".jpe", ".jpg", ".mac", ".pbm",
+                ".pcd", ".pcx", ".pct", ".pgm", ".png", ".ppm", ".psd", ".ras", ".tga", ".tif", ".wmf"]
+AUDIO_FORMAT = [".3gp", ".aa", ".aac", ".aax", ".act", ".aiff", ".amr", ".ape", ".au", ".awb", ".dct", ".dss", ".dvf",
+                ".flac", ".gsm", ".iklax", ".ivs", ".m4a", ".m4b", ".m4p", ".mmf", ".mogg", ".mp3", ".mpc", ".msv",
+                ".oga", ".ogg", ".opus", ".ra", ".raw", ".rm", ".sln", ".tta", ".vox", ".wav", ".webm", ".wma", ".wv"]
+
+
+class FileSystem(object):
+    def __init__(self, path):
+        """
+        :param path: string
+            Path to file
+        """
+        object.__init__(self)
+
+        self.path = path
+        self.name, self.extension = os.path.splitext(self.path)
+
+    def is_russian(self):
+        """
+        :return: True iff document has a russian name.
+        """
+
+        russian_chars = 0
+        for c in RUSSIAN_CHARS:
+            if c in self.name:
+                russian_chars += 1  # found a russian char
+        return russian_chars > len(RUSSIAN_CHARS) / 2.0
+
+    def remove_year(self):
+        """
+        :return: string
+            Given string bu with no years.
+        """
+
+        l_bracket, r_bracket = self.name.find("("), self.name.find(")")  # find limits of year
+        if r_bracket - l_bracket + 1 == 6:  # there is a year in between
+            name = self.name.replace(self.name[l_bracket: r_bracket + 1], "")  # remove year
+        else:
+            l_bracket, r_bracket = self.name.find("["), self.name.find("]")  # try with square brackets
+            if r_bracket - l_bracket + 1 == 6:  # there is a year in between
+                name = self.name.replace(self.name[l_bracket: r_bracket + 1], "")  # remove year
+
+        for i in range(len(self.name) - 4):
+            try:
+                if name[i: i + 4].isdigit():
+                    name = name[:i] + name[i + 4:]
+            except:  # out of bounds
+                pass
+
+        return name
+
+    def remove_brackets(self):
+        """
+        :return: string
+            Given string bu with no barckets.
+        """
+
+        name = re.sub("([\(\[]).*?([\)\]])", "\g<1>\g<2>", self.name)
+        name = name.replace("()", "")  # remove anything in ()
+        if name.rfind("(") > 0:  # there exists a "("
+            name = name[:name.rfind("(")]
+        name = name.replace("(", "")
+        name = name.replace(")", "")
+        for _ in range(10):
+            name = name.replace("  ", " ")  # remove extra blanks
+        name = name.strip()
+        return name
+
+    def extract_name_max_chars(self, max_chars, blank=" "):
+        """
+        :param max_chars: int
+            Maximum chars of new name
+        :param blank: string
+            Char that represents the blank between words.
+        :return: string
+            Name edited to contain at most max_chars (truncate to nearest word)
+        """
+
+        if len(self.name) > max_chars:
+            new_name = self.name[:max_chars]  # get at most 64 chars
+            if new_name.rfind(blank) > 0:
+                new_name = new_name[:new_name.rfind(blank)]  # truncate to nearest word
+            return new_name
+        else:
+            return self.name
+
+    def prettify(self, r=" "):
+        """
+        :param r: string
+            Default blanks in name.
+        :return: string
+            Prettier name from given one: replace bad chars with good ones.
+        """
+
+        if self.name.startswith("."):  # remove starting .
+            name = self.name[1:]
+        else:
+            name = self.name
+
+        for t in BAD_CHARS:
+            name = name.replace(t.lower(), r)  # remove token
+        name = name.replace(" ", r)  # replace blanks
+        while name.find(r + r) > 0:  # while there are blanks to remove
+            name = name.replace(r + r, r)
+
+        for i in range(1, len(name) - 2):  # loop through characters except 1 and end
+            try:
+                if name[i - 1] == r and name[i + 1] == r:  # 2 replacement hug one good char
+                    name = name[:i - 1] + name[i + 1:]
+            except:  # out of bounds
+                pass
+
+        if name.endswith(r):  # remove ending replacement
+            name = name[:-1]
+
+        return name
+
+    def trash(self):
+        """
+        :return: void
+            Trash given file/folder
+        """
+
+        send2trash(self.path)
+
+
+class Document(FileSystem):
+    def __init__(self, path):
+        """
+        :param path: string
+            Path to file
+        """
+
+        FileSystem.__init__(self, path)
+
+    def get_path_name(self):
+        """
+        :return: tuple string, string
+            Name of path, name of file (or folder)
+        """
+
+        p, name = os.path.dirname(os.path.abspath(self.path)), os.path.basename(self.path)
+        return p, name
+
+    def is_video(self):
+        """
+        :return: True iff document is a video.
+        """
+        return self.extension.lower() in VIDEO_FORMAT
+
+    def is_subtitle(self):
+        """
+        :return: True iff document is a subtitle.
+        """
+        
+        return self.extension.lower() in SUBTITLE_FORMAT
+
+    def is_text(self):
+        """
+        :return: True iff document is a text file.
+        """
+        
+        return self.extension.lower() in TEXT_FORMAT
+
+    def is_image(self):
+        """
+        :return: True iff document is an image.
+        """
+        
+        return self.extension.lower() in IMAGE_FORMAT
+
+    def is_audio(self):
+        """
+        :return: True iff document is an audio.
+        """
+        
+        return self.extension.lower() in AUDIO_FORMAT
+
+    def is_archive_mac(self):
+        """
+        :return: True iff document is an MACOSX archive.
+        """
+
+        return "macosx" in self.path.lower()
+
+    def is_hidden(self):
+        """
+        :return: bool
+            True iff path is hidden
+        """
+
+        hidden_start_path = os.path.pathsep() + "."
+        return hidden_start_path in self.path
+
     @staticmethod
     def move_file_to_directory(file_path, directory_path):
         """
@@ -34,7 +251,6 @@ class Document:
         :return: void
             Move file to given directory
         """
-
         file_name = os.path.basename(file_path)  # get name of file
         if not os.path.exists(directory_path):
             os.makedirs(directory_path)  # create directory if necessary
@@ -50,7 +266,6 @@ class Document:
         :return: void
             Move file from old location to new one
         """
-
         try:
             os.rename(old_path, new_path)
         except:
@@ -75,7 +290,6 @@ class Document:
         :return: void
             Writes given data to given path file.
         """
-
         with open(out_file, "w") as f:
             f.write(data)
 
@@ -87,11 +301,28 @@ class Document:
         :return: tuple string, string
             Name of file, extension of file
         """
-
         return os.path.splitext(file_name)
 
 
-class Directory:
+class Directory(FileSystem):
+    def __init__(self, path):
+        """
+        :param path: string
+            Path to file
+        """
+
+        FileSystem.__init__(self, path)
+
+    def get_path_name(self):
+        """
+        :return: tuple string, string
+            Name of path, name of file (or folder)
+        """
+
+        p = os.path.dirname(os.path.abspath(self.path))
+        name = self.path.replace(p + os.path.sep, "")[: -1]  # replace in full path, dir path to get name
+        return p, name
+
     @staticmethod
     def ls_dir(path, include_hidden=False):
         """
@@ -105,7 +336,7 @@ class Directory:
 
         list_ = []
         for f in os.listdir(path):
-            if include_hidden or not is_hidden(f):
+            if include_hidden or not Document(f).is_hidden():
                 list_.append(os.path.join(path, f))
         return list_
 
@@ -122,7 +353,7 @@ class Directory:
 
         list_ = []
         for f in os.listdir(path):
-            if include_hidden or not is_hidden(f):
+            if include_hidden or not Document(f).is_hidden():
                 list_.append(os.path.join(path, f))
                 if os.path.isdir(os.path.join(path, f)):
                     list_ += Directory.ls_recurse(os.path.join(path, f),
@@ -141,7 +372,6 @@ class Directory:
         :return: list
             List of paths in given directory recursively.
         """
-
         if recurse:
             return Directory.ls_recurse(path, include_hidden=include_hidden)
         else:
--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ setup(
         "sklearn",
         "requests",
         "lxml",
-        "scipy"
+        "scipy",
+        "send2trash"
     ]
 )
