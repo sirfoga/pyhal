e9c7725d4b799a4f26c481148cf05f98d576f378
Stefano Fogarollo <sirfoga@protonmail.com>
2017-10-22 15:21:38 +0200

    update copyright

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # !/usr/bin/python3
 # coding: utf_8
 
-# Copyright 2016 Stefano Fogarollo
+# Copyright 2016-2018 Stefano Fogarollo
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
