4bc642e34a580568252d9c63b259b6102474a897
stefano <sirfoga@protonmail.com>
2018-07-25 21:50:03 +0200

    hide output when generating docs

--- docs/make.sh
+++ docs/make.sh
@@ -9,7 +9,7 @@ OUTPUT_FOLDER="doxygen/html/"
 DOCS_FOLDER="docs/"
 DOCS_BRANCH="gh-pages"
 BUILD_FOLDER="build/"
-BUILD_COMMAND="doxygen Doxyfile"
+BUILD_COMMAND="doxygen doxygen/Doxyfile > /dev/null 2>&1"  # hide output
 COMMIT_MSG=$(git log -1 --pretty=%B)  # last commit message
 COMMIT_MSg="${COMMIT_MSG} (generated docs)"  # add docs notice
 
@@ -17,20 +17,22 @@ echo "> always remember to commit any changes before checking out branches"
 echo "> cleaning..."
 rm -rf ${OUTPUT_FOLDER}  # clean
 rm -rf ${BUILD_FOLDER}
+mkdir ${BUILD_FOLDER}
 
 echo "> generating..."
-# todo eval ${BUILD_COMMAND}  # make docs
-# todo mv ${OUTPUT_FOLDER}* ${BUILD_FOLDER}  # move to build folder
+eval ${BUILD_COMMAND}  # make docs
+mv -v ${OUTPUT_FOLDER}* ${BUILD_FOLDER}  # move to build folder
 
 echo "> moving to docs branch"
 git checkout ${DOCS_BRANCH}  # change branch (to publish docs)
-# todo mv ${BUILD_FOLDER}* ../  # move to root
+mv ${BUILD_FOLDER}* ../  # move to root
 
 rm -rf ${BUILD_FOLDER}  # clean
+rm -rf ${OUTPUT_FOLDER}
 
 echo "> committing changes"
-# todo git add --all
-# todo git commit -m "${COMMIT_MSG} (generated docs)"
+git add --all
+git commit -m "${COMMIT_MSG} (generated docs)"
 
 echo "> exiting..."
-git checkout master
+# todo git checkout master
