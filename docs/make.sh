263edf80362f856c76edb435acb3ddffe930b239
stefano <sirfoga@protonmail.com>
2018-07-25 21:15:50 +0200

    fix docs generators

--- docs/make.sh
+++ docs/make.sh
@@ -1,46 +1,36 @@
 #!/usr/bin/env bash
 # coding: utf_8
 
-# Copyright 2016-2018 Stefano Fogarollo
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+# Generates docs in correct git branch
+# Commits changes in that branch
+# Returns to main branch
 
 OUTPUT_FOLDER="doxygen/html/"
 DOCS_FOLDER="docs/"
+DOCS_BRANCH="gh-pages"
 BUILD_FOLDER="build/"
 BUILD_COMMAND="doxygen Doxyfile"
 COMMIT_MSG=$(git log -1 --pretty=%B)  # last commit message
 COMMIT_MSg="${COMMIT_MSG} (generated docs)"  # add docs notice
 
-echo "\t\t<<<<\tCLEANING\t>>>>"
+echo "> always remember to commit any changes before checking out branches"
+echo "> cleaning..."
 rm -rf ${OUTPUT_FOLDER}  # clean
 rm -rf ${BUILD_FOLDER}
-mkdir ${BUILD_FOLDER}  # prepare build folder
 
-echo "        <<<<    GENERATING DOCS    >>>>"
-eval ${BUILD_COMMAND}  # make docs
-mv ${OUTPUT_FOLDER}* ${BUILD_FOLDER}  # move to build folder
+echo "> generating..."
+# todo eval ${BUILD_COMMAND}  # make docs
+# todo mv ${OUTPUT_FOLDER}* ${BUILD_FOLDER}  # move to build folder
 
-echo "        <<<<    MOVING TO GH-PAGES    >>>>"
-git checkout gh-pages  # change branch (to publish docs)
-mv ${BUILD_FOLDER}* ../
+echo "> moving to docs branch"
+# todo git checkout ${DOCS_BRANCH}  # change branch (to publish docs)
+# todo mv ${BUILD_FOLDER}* ../  # move to root
 
 rm -rf ${BUILD_FOLDER}  # clean
 
-echo "        <<<<    COMMITTING    >>>>"
-git add --all
-git commit -m "${COMMIT_MSG} (generated docs)"
+echo "> committing changes"
+# todo git add --all
+# todo git commit -m "${COMMIT_MSG} (generated docs)"
 
-echo "        <<<<    GETTING BACK TO MASTER    >>>>"
+echo "> exiting..."
 git checkout master
