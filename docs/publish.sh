0eb574afeb8b2a54b2efa7ace257906ff03050bd
stefano <sirfoga@protonmail.com>
2018-07-25 21:59:10 +0200

    fix docs publisher

--- docs/publish.sh
+++ docs/publish.sh
@@ -4,8 +4,6 @@
 # Pushes changes in docs branch
 # Returns to main branch
 
-chmod +x make.sh
-
 echo "> always remember to commit any changes before checking out branches"
 echo "> moving to docs branch"
 git checkout gh-pages  # change branch (to publish docs)
