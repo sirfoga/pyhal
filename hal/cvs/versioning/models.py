0466eee25aaceac33385d32c1e6b33d1e44e1130
stefano <sirfoga@protonmail.com>
2018-07-30 15:27:17 +0200

    add versioning module

---
+++ hal/cvs/versioning/models.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+""" Models to version stuff """
+
+
+class Level:
+    """ Level of version number """
+
+
+class Subsystem:
+    """ List of levels of version system """
+
+
+class VersionNumber:
+    """ Version """
---
+++ hal/cvs/versioning/semantic.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+""" Models for semantic versioning (see https://semver.org/) """
