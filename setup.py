7216c524d08ba82cc88a9bb41d021aed6d6703d3
stefano <sirfoga@protonmail.com>
2018-07-30 20:12:03 +0200

    update marketing

--- setup.py
+++ setup.py
@@ -9,23 +9,14 @@ from setuptools import setup, find_packages
 
 DESCRIPTION = \
     "HAL\n\n\
-    Handy Algorithmic Library: a multipurpose library homemade from " + \
-    "scratch to perform great stuff in a pythonic way.\n\
-    \n\
-    Examples\n\n\
-    You can take a look at my other repository: there are lots of " + \
-    "implementations from various HAL modules.\n\
+    A multipurpose library homemade from scratch to perform great stuff in a pythonic way\n\
     \n\
     Install\n\n\
-    - $ python3 setup.py install from the source\n\
-    - $ pip3 install pyhal via pip\n\
+    - $ pip3 install . --upgrade --force-reinstall, from the source\n\
+    - $ pip3 install pyhal, via pip\n\
     \n\
     Questions and issues\n\n\
-    The Github issue tracker is only for bug reports and feature requests." \
-    "Anything else, such as questions for help in using the library, " + \
-    "should be posted in StackOverflow.\n\
-    \n\
-    License: Apache License Version 2.0, January 2004"
+    The Github issue tracker is only for bug reports and feature requests."
 
 
 setup(
