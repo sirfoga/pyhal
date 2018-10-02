f67d93de2c28fb17c0d36eecf8f6197c0af195dd
sirfoga <sirfoga@protonmail.com>
2018-10-02 19:05:38 +0200

    add common finder

--- hal/data/lists.py
+++ hal/data/lists.py
@@ -23,3 +23,36 @@ def normalize_array(arr):
     np_arr = np.array(arr)
     x_normalized = np_arr / np_arr.max(axis=0)
     return list(x_normalized)
+
+
+def is_in_all(value, lst):
+    """
+    :param value: anything
+        Value to check
+    :param lst: [] of []
+        List of lists
+    :return: bool
+        True iff value is in all inner lists
+    """
+
+    for l in lst:
+        if value not in l:
+            return False
+
+    return True
+
+
+def find_commons(lst):
+    """
+    :param lst: [] of []
+        List of lists
+    :return: []
+        List of values that are in common between inner lists
+    """
+
+    others = lst[1:]
+    return [
+        val
+        for val in lst[0]
+        if is_in_all(val, others)
+    ]
