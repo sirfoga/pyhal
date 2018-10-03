31e0fc3837e4a6464c684f97dd7a9ca103778dc0
sirfoga <sirfoga@protonmail.com>
2018-10-03 09:53:59 +0200

    fix fast installer

--- fast_install.sh
+++ fast_install.sh
@@ -1,6 +1,11 @@
 # !/bin/bash
 # coding: utf_8
 
-LOCAL_FOLDER="hal"
-DIST_FOLDER="/usr/local/lib/python3.6/dist-packages/hal"
-sudo cp -r ${LOCAL_FOLDER} ${DIST_FOLDER}  # copy recursively
+LOCAL_FOLDER="${PWD}/hal/*"
+PYTHON_VERSION="python3.6"
+DIST_FOLDER="/usr/local/lib/${PYTHON_VERSION}/dist-packages/hal"
+
+rm -rf ${DIST_FOLDER}  # clean
+mkdir ${DIST_FOLDER}
+cp -r ${LOCAL_FOLDER} ${DIST_FOLDER}  # copy recursively
+echo "Installed to ${DIST_FOLDER}"
