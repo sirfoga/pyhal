ee9792b696c878efd29e58926f6a475ca9e0d355
sirfoga <sirfoga@protonmail.com>
2018-10-04 18:33:39 +0200

    add maths.utils module

---
+++ hal/maths/utils.py
@@ -0,0 +1,12 @@
+# !/usr/bin/python3
+# coding: utf-8
+
+""" Tools realtive to maths """
+
+
+def get_percentage_relative_to(val, other):
+    val = float(val)
+    other = float(other)
+    ratio = val / other - 1
+
+    return ratio * 100.0
