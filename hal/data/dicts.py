3baea3ca786cb09d1d920d89d24d214029cf2864
sirfoga <sirfoga@protonmail.com>
2018-10-07 19:48:57 +0200

    remove dicts module

--- hal/data/dicts.py
+++ hal/data/dicts.py
@@ -6,18 +6,18 @@
 
 import numpy as np
 
-from hal.strings.utils import how_similar_are
+from strings.utils import how_similar_are
 
 
 def how_similar_dicts(d1, d2):
     """
+    Calculates similarity
+
     Args:
       d1: Dictionary
       d2: Dictionary
 
-    Returns:
-      float in [0 - 1]
-      A measure of how much similar values of dictionaries are
+    Returns: A measure of how much similar values of dictionaries are
     """
     values = []
     for k in d1:  # iterate keys
