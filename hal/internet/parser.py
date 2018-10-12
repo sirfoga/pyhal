1e6a8b1cf583364dffb5186d9974302dff6728bc
sirfoga <sirfoga@protonmail.com>
2018-10-12 18:39:36 +0200

    refactor HTML row parse

--- hal/internet/parser.py
+++ hal/internet/parser.py
@@ -18,30 +18,59 @@ class HtmlTable:
         self.source = html_source
         self.soup = BeautifulSoup(self.source, "lxml")
 
+    @staticmethod
+    def _get_row_tag(row, tag):
+        """Parses row and gets columns matching tag
+
+        :param row: HTML row
+        :param tag: tag to get
+        :return: list of labels in row
+        """
+
+        is_empty = True
+        data = []
+        for column_label in row.find_all(tag):  # cycle through all labels
+            data.append(
+                html_stripper(column_label.text)
+            )
+            if data[-1]:
+                is_empty = False
+
+        if not is_empty:
+            return data
+
+        return None
+
+    @staticmethod
+    def _parse_row(row):
+        """Parses HTML row
+
+        :param row: HTML row
+        :return: list of values in row
+        """
+
+        data = []
+
+        labels = HtmlTable._get_row_tag(row, "th")
+        if labels:
+            data += labels
+
+        columns = HtmlTable._get_row_tag(row, "td")
+        if columns:
+            data += columns
+
+        return data
+
     def parse(self):
         """Parses data in table
 
         :return: List of list of values in table
         """
         data = []  # add name of section
+
         for row in self.soup.find_all("tr"):  # cycle through all rows
-            is_empty = True
-            data_row = []
+            parsed = self._parse_row(row)
+            if parsed:
+                data.append(parsed)
 
-            for column_label in row.find_all("th"):  # cycle through all labels
-                data_row.append(
-                    html_stripper(column_label.text)
-                )
-                if data_row[-1]:
-                    is_empty = False
-
-            for column in row.find_all("td"):  # cycle through all columns
-                data_row.append(
-                    html_stripper(column.text)
-                )
-                if data_row[-1]:
-                    is_empty = False
-
-            if not is_empty:
-                data.append(data_row)
         return data
